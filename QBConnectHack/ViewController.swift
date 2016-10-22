ab1ad94d4a62b526f359f20dc0131b910f0808e8
Saumeel Gajera <saumeel@Saumeels-MacBook-Pro.local>
2016-10-22 14:40:10 -0700

    registration w/o mongo setup

--- QBConnectHack/ViewController.swift
+++ QBConnectHack/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var emailID: UITextField!
+    @IBOutlet weak var contactNumber: UITextField!
+    
+    
+    
+    @IBAction func submit(){
+        // make a n/w call to send the data
+        print("firstName : \(firstName.text)")
+        print("lastName : \(lastName.text)")
+        print("Email ID  : \(emailID.text)")
+        print("contact No : \(contactNumber.text)")
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
