2d83a1bbeb65a38fd669a375a569269fe604267f
Saumeel Gajera <saumeel@Saumeels-MacBook-Pro.local>
2016-10-22 18:34:10 -0700

    parse updated

--- QBConnectHack/AppDelegate.swift
+++ QBConnectHack/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         UserDO.registerSubclass()
         Parse.initializeWithConfiguration(
             ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
